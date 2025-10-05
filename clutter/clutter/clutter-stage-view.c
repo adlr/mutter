41e6ebe0134d15af146739d411db8e7feb366ea8
Andrew de los Reyes <adlr@gmail.com>
2025-10-05 16:46:55 -0700

    DEBUG: Add some debug logs

--- clutter/clutter/clutter-stage-view.c
+++ clutter/clutter/clutter-stage-view.c
@@ -832,6 +832,8 @@ clutter_stage_view_add_redraw_clip (ClutterStageView   *view,
       maybe_mark_full_redraw (view, &priv->redraw_clip);
     }
 
+  g_warning("clutter_stage_view_add_redraw_clip %p %d %d %d %d set to true",
+            view, clip->x, clip->y, clip->width, clip->height);
   priv->has_redraw_clip = TRUE;
 }
 
