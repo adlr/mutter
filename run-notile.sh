d87c72b1c4c5e9a63e7faccbd8c8e3d912ac9a5c
Andrew de los Reyes <adlr@gmail.com>
2025-10-18 11:37:15 -0700

    DEBUG: useful scripts for local building and testing
    Needs this installed:
    
    git clone https://github.com/ianlancetaylor/libbacktrace.git
    ./configure && make && sudo make install

---
+++ run-notile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm /var/run/user/1000/mutter-bt.log
+CLUTTER_DEBUG="frame-clock,frame-timings" MUTTER_DEBUG="backend,kms" dbus-run-session mutter --wayland vte-2.91 &> "${MUTTER_LOGFILE:-mutter-notile.log}"
+# Valgrind variant:
+#CLUTTER_DEBUG="frame-clock,frame-timings" MUTTER_DEBUG="backend,kms" dbus-run-session /usr/bin/valgrind --log-file=mutter-valgrind.txt --enable-debuginfod=no --leak-check=full --show-leak-kinds=definite mutter --wayland vte-2.91 &> "${MUTTER_LOGFILE:-mutter-notile.log}"
