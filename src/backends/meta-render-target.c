f6a2cba2f764691e516a25e178b22e899ac07a4b
Andrew de los Reyes <adlr@gmail.com>
2025-11-28 16:09:09 -0800

    TILETEAR: Add MetaRenderTarget[Native] classes

---
+++ src/backends/meta-render-target.c
@@ -0,0 +1,144 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/*
+ * Copyright (C) 2025 ?
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * MetaRendererTarget:
+ *
+ * Holds a collection of MetaCrtc and MetaOutput pointers that represent
+ * a single output. The common case is 1 of each. In the case of a tiled
+ * display, objects of this class will hold 1 crtc and output per tile.
+ * The first element of each member array is the "primary" one.
+ * There must be at least one crtc and output, and the number of
+ * crtcs and outputs must be identical.
+ */
+
+#include "config.h"
+
+#include "backends/meta-render-target.h"
+
+#include <glib-object.h>
+
+struct _MetaRenderTarget
+{
+  GObject parent_instance;
+
+  GPtrArray *crtcs;  /* of type MetaCrtc * */
+  GPtrArray *outputs;  /* of type MetaOutput * */
+};
+
+G_DEFINE_TYPE (MetaRenderTarget, meta_render_target, G_TYPE_OBJECT)
+
+void
+meta_render_target_add (MetaRenderTarget *render_target,
+                        MetaCrtc *crtc,
+                        MetaOutput *output)
+{
+  g_ptr_array_add (render_target->crtcs, g_object_ref (crtc));
+  g_ptr_array_add (render_target->outputs, g_object_ref (output));
+}
+
+MetaCrtc *
+meta_render_target_get_primary_crtc (MetaRenderTarget *render_target)
+{
+  g_warn_if_fail (render_target->crtcs->len > 0);
+  return g_ptr_array_index (render_target->crtcs, 0);
+}
+
+MetaOutput *
+meta_render_target_get_primary_output (MetaRenderTarget *render_target)
+{
+  g_warn_if_fail (render_target->outputs->len > 0);
+  return g_ptr_array_index (render_target->outputs, 0);
+}
+
+const char *
+meta_render_target_get_name (MetaRenderTarget *render_target)
+{
+  MetaOutput *primary_output = meta_render_target_get_primary_output (render_target);
+  return meta_output_get_name (primary_output);
+}
+
+GPtrArray *
+meta_render_target_get_crtcs (MetaRenderTarget *render_target)
+{
+  return render_target->crtcs;
+}
+
+GPtrArray *
+meta_render_target_get_outputs (MetaRenderTarget *render_target)
+{
+  return render_target->outputs;
+}
+
+MtkRectangle
+meta_render_target_get_view_layout (MetaRenderTarget *render_target)
+{
+  MtkRectangle view_layout;
+  g_warn_if_fail (render_target->crtcs->len > 0);
+  const MetaCrtcConfig *crtc_config = meta_crtc_get_config (g_ptr_array_index (render_target->crtcs, 0));
+  mtk_rectangle_from_graphene_rect (&crtc_config->layout,
+                                    MTK_ROUNDING_STRATEGY_ROUND,
+                                    &view_layout);
+  // Handle all crtcs after the first by unioning them together
+  for (guint i = 1; i < render_target->crtcs->len; i++)
+    {
+      MetaCrtc *crtc = g_ptr_array_index (render_target->crtcs, i);
+      MtkRectangle other_view_layout;
+      crtc_config = meta_crtc_get_config (crtc);
+      mtk_rectangle_from_graphene_rect (&crtc_config->layout,
+                                        MTK_ROUNDING_STRATEGY_ROUND,
+                                        &other_view_layout);
+      mtk_rectangle_union (&view_layout, &other_view_layout, &view_layout);
+    }
+  return view_layout;
+}
+
+MetaGpu *
+meta_render_target_get_gpu (MetaRenderTarget *render_target)
+{
+  return meta_crtc_get_gpu (meta_render_target_get_primary_crtc (render_target));
+}
+
+static void
+meta_render_target_dispose (GObject *object)
+{
+  MetaRenderTarget *render_target = META_RENDER_TARGET (object);
+
+  g_ptr_array_foreach (render_target->crtcs, (GFunc) g_object_unref, NULL);
+  g_ptr_array_unref (render_target->crtcs);
+  g_ptr_array_foreach (render_target->outputs, (GFunc) g_object_unref, NULL);
+  g_ptr_array_unref (render_target->outputs);
+
+  G_OBJECT_CLASS (meta_render_target_parent_class)->dispose (object);
+}
+
+static void
+meta_render_target_init (MetaRenderTarget *render_target)
+{
+  render_target->crtcs = g_ptr_array_new ();
+  render_target->outputs = g_ptr_array_new ();
+}
+
+static void
+meta_render_target_class_init (MetaRenderTargetClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = meta_render_target_dispose;
+}
