6eab315084fe28e494d98f2b528bd14fded9b70e
Andrew de los Reyes <adlr@gmail.com>
2025-12-14 16:52:42 -0800

    better debug

--- src/backends/native/meta-renderer-native.c
+++ src/backends/native/meta-renderer-native.c
@@ -1225,8 +1225,12 @@ meta_renderer_native_queue_modes_reset (MetaRendererNative *renderer_native)
               clutter_stage_view_get_layout (stage_view, &view_layout);
               view_scale = clutter_stage_view_get_scale (stage_view);
 
-              meta_topic (META_DEBUG_KMS, "output_layout %d %d, view_layout %d %d",
-                output_layout.width, output_layout.height, view_layout.width, view_layout.height);
+              meta_topic (META_DEBUG_KMS, "output_layout %d %d %d %d, view_layout %d %d %d %d scale %f",
+                output_layout.x, output_layout.y,
+                output_layout.width, output_layout.height,
+                view_layout.x, view_layout.y,
+                view_layout.width, view_layout.height,
+                view_scale);
               crtc_layout = (MetaKmsCrtcLayout) {
                 .crtc = kms_crtc,
                 .cursor_plane = kms_plane,
