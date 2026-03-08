aa04fe9f050bd491651ff13fff39d8b60d6ddb3e
Andrew de los Reyes <adlr@chromium.org>
2026-03-07 19:17:31 -0800

    fixup: copyright headers (MetaRenderTarget[Native])

--- src/backends/meta-render-target.h
+++ src/backends/meta-render-target.h
@@ -1,7 +1,7 @@
 /* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
 
 /*
- * Copyright (C) 2025 ?
+ * Copyright (C) 2026 the Mutter authors
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
